ffb396244694c1e8db2313b45faddd19fce29cea
Dino Scheidt <github@din.ooo>
2025-01-28 01:40:32 +0100

    🔨 Factor out public folder

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,6 +1,7 @@
 // App.tsx
 import { CornerPinImage } from "./components/CornerPinImage.tsx";
-import alignmentImage from "./assets/projection/ipad_alignment_portrait.png"; // Ensure this path is correct
+import alignmentImage from "#assets/projection/ipad_alignment_portrait.png";
+import reactLogo from "#assets/react.svg";
 
 function App() {
   // Example corners in percent of screen size:
@@ -12,13 +13,23 @@ function App() {
   };
 
   return (
-    <CornerPinImage
-      src={alignmentImage}
-      corners={corners}
-      srcWidth={1366} // e.g. after you physically rotated the image to landscape
-      srcHeight={1024} // ...
-      backgroundColor="#8d8d8d"
-    />
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <CornerPinImage
+        src={alignmentImage}
+        corners={corners}
+        srcWidth={1366} // e.g. after you physically rotated the image to landscape
+        srcHeight={1024} // ...
+        backgroundColor="#8d8d8d"
+      />
+    </>
   );
 }
 
--- browser/vite.config.ts
+++ browser/vite.config.ts
@@ -6,11 +6,13 @@ import { resolve } from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  publicDir: resolve(__dirname, "../assets/public"),
   resolve: {
     alias: {
       // Needs to be identical to deno.jsonc
       "#lib": resolve(__dirname, "../shared/lib"),
       "#types": resolve(__dirname, "../shared/types"),
+      "#assets": resolve(__dirname, "../assets"),
     },
   },
 });

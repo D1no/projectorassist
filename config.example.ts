abbe87651418f4248b7791fee0079cd2d001227c
Dino Scheidt <github@din.ooo>
2025-02-10 00:41:10 +0100

    💡 Update example to keep in sync

--- config.example.ts
+++ config.example.ts
@@ -11,3 +11,17 @@ export let SERVER_BASE_URL: string; // = "http://ipaddress";
  * Change to the port the server is running at. Needs to be in sync with `server/deno.jsonc`.
  */
 export let SERVER_PORT: number; // = 3001;
+
+/**
+ * Location of the slides folder in the assets/public.
+ */
+export const SLIDES_PUBLIC_FOLDER: string = "/slides/";
+
+/**
+ * Must be in the assets/public/slides folder.
+ */
+export const SLIDES: string[] = [
+  "0_alignment_ipad_2022_12inch_portrait.png",
+  // "my_slide_01.png",
+  // ...
+];
